801c180934a30505dcacff900e2c0374e4aeffdb
shafik720 <shafikrasel5@gmail.com>
2023-11-25 18:23:31 +0600

    server trial running successfully

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express' ;
+import cors from 'cors' ; 
+
+const app : Application = express() ; 
+
+// --- parser 
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server Running Successfully ') ; 
+}) 
+
+export default app ; 
